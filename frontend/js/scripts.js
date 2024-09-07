b5a43fe3215abbd2962ae154336e86e278de712f
Lê Alves <leticiaviz.alves@gmail.com>
2024-09-07 19:22:06 -0300

    Modais

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -11,4 +11,76 @@ window.onload = function() {
     //Apresenta no campo
     dataInput.value = mes + '/' + ano;
 
+    // Máscara para o campo de data do modal
+    var dataModalInput = document.getElementById("dataModal");
+    VMasker(dataModalInput).maskPattern("99/99/9999"); // Mesma máscara para o modal
+
+    // Lógica de abertura e fechamento do modal
+    var modal = document.getElementById("modalAgendamento");
+    var btn = document.getElementById("agendarBtn");
+    var span = document.getElementsByClassName("close")[0];
+    var cancelarBtn = document.getElementById("cancelarBtn");
+
+    // Abrir o modal ao clicar no botão "Agendar Compromisso"
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Fechar o modal ao clicar no "X"
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Fechar o modal ao clicar no "Cancelar"
+    cancelarBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Fechar o modal ao clicar fora dele
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+      // Lógica para abrir e fechar o modal de edição
+    var modalEdicao = document.getElementById("modalEdicao");
+    var btnFecharEdicao = document.getElementsByClassName("close")[1]; // Segundo botão "fechar" do modal de edição
+    var cancelarEdicaoBtn = document.getElementById("cancelarEdicaoBtn");
+    var editarBtn = document.getElementById("editarBtn"); // Botão temporário de edição
+
+    // Função para abrir o modal de edição
+    function abrirModalEdicao() {
+        modalEdicao.style.display = "block";
+    }
+
+    // Função para fechar o modal de edição
+    function fecharModalEdicao() {
+        modalEdicao.style.display = "none";
+    }
+
+    // Abrir o modal ao clicar no botão "Abrir Modal de Edição"
+    editarBtn.onclick = function() {
+        abrirModalEdicao();
+    }
+
+    // Fechar o modal ao clicar no botão "fechar" (X)
+    btnFecharEdicao.onclick = function() {
+        fecharModalEdicao();
+    }
+
+    // Fechar o modal ao clicar no botão "Cancelar"
+    cancelarEdicaoBtn.onclick = function() {
+        fecharModalEdicao();
+    }
+
+    // Fechar o modal ao clicar fora dele
+    window.onclick = function(event) {
+        if (event.target == modalEdicao) {
+            fecharModalEdicao();
+        }
+    }
+
+
+
 };
